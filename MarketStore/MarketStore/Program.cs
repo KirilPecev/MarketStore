4f7950621c275c9c4b19fb05179d9441de020dac
Kiril Pecev <kir4o_pecev@abv.bg>
2020-10-26 14:35:33 +0200

    added exception handling

--- MarketStore/MarketStore/Program.cs
+++ MarketStore/MarketStore/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace MarketStore
 {
     using System;
+    using System.IO;
 
     public class Program
     {
@@ -9,22 +10,41 @@
             Console.Write("Owner: ");
             string owner = Console.ReadLine();
 
-            Console.Write("Turnover: ");
-            decimal turnover = decimal.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Turnover: ");
+                decimal turnover = decimal.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
-            Console.Write("Purchase value: ");
-            decimal purchaseValue = decimal.Parse(Console.ReadLine());
+                Console.Write("Purchase value: ");
+                decimal purchaseValue = decimal.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
-            Console.WriteLine(new string('-', 30));
+                Console.WriteLine(new string('-', 30));
 
-            Card card = new GoldCard(owner, turnover);
-            decimal discount = card.CalculateDiscount(purchaseValue);
-            decimal total = purchaseValue - discount;
+                Card card = new GoldCard(owner, turnover);
+                decimal discount = card.CalculateDiscount(purchaseValue);
+                decimal total = purchaseValue - discount;
 
-            Console.WriteLine($"Purchase value: ${purchaseValue:f2}");
-            Console.WriteLine($"Discount rate: {card.DiscountRate:f1}%");
-            Console.WriteLine($"Discount: ${discount:f2}");
-            Console.WriteLine($"Total: ${total:f2}");
+                Console.WriteLine($"Purchase value: ${purchaseValue:f2}");
+                Console.WriteLine($"Discount rate: {card.DiscountRate:f1}%");
+                Console.WriteLine($"Discount: ${discount:f2}");
+                Console.WriteLine($"Total: ${total:f2}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter the correct values!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Invalid operation! Please enter the correct values!");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Output exception!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong. Please contact with the admin!");
+            }
         }
     }
 }
